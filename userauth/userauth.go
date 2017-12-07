4d69c0a763dfbce4c519012a17b5ae8cc84e6603
Takahiro Yamazaki <yamazaki@nvs-one.com>
2017-12-07 16:17:37 +0900

    add userauth

---
+++ userauth/userauth.go
@@ -0,0 +1,28 @@
+package userauth
+
+import (
+	"fmt"
+
+	"github.com/BurntSushi/toml"
+	oauth2 "google.golang.org/api/oauth2/v2"
+)
+
+type UserAuth struct {
+	UserList []string
+}
+
+func NewUserAuth(path string) *UserAuth {
+	var config UserAuth
+	toml.DecodeFile(path, &config)
+	fmt.Println(config)
+	return &config
+}
+
+func (l *UserAuth) Authentication(token *oauth2.Tokeninfo) bool {
+	for _, v := range l.UserList {
+		if v == token.Email {
+			return true
+		}
+	}
+	return false
+}
